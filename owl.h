60ffe0451942f325240f19bf245ba511a6ba5ca7
James M. Kretchmar <kretch@mit.edu>
2002-08-18 11:44:06 +0000

    Oops, version bumped to 1.2.3 and ChangeLog updated accordingly.

--- owl.h
+++ owl.h
@@ -11,8 +11,8 @@
 
 static const char owl_h_fileIdent[] = "$Id$";
 
-#define OWL_VERSION         1.2.2
-#define OWL_VERSION_STRING "1.2.2"
+#define OWL_VERSION         1.2.3
+#define OWL_VERSION_STRING "1.2.3"
 
 #define OWL_DEBUG 0
 #define OWL_DEBUG_FILE "/var/tmp/owldebug"
