24dc9ce886d481f61b27dab521b316d9ea84f788
James M. Kretchmar <kretch@mit.edu>
2003-12-25 16:40:29 +0000

    Updated version number and collapsed change log for release

--- owl.h
+++ owl.h
@@ -19,8 +19,8 @@
 
 static const char owl_h_fileIdent[] = "$Id$";
 
-#define OWL_VERSION         2.1.1-pre-3
-#define OWL_VERSION_STRING "2.1.1-pre-3"
+#define OWL_VERSION         2.1.1
+#define OWL_VERSION_STRING "2.1.1"
 
 /* Feature that is being tested to redirect stderr through a pipe. 
  * There may still be some portability problems with this. */
