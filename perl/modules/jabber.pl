d0b3a403de3b766d74864628064a57c7cb2e8c84
Nelson Elhage <nelhage@mit.edu>
2006-12-24 23:23:29 +0000

    r15871@phanatique:  nelhage | 2006-12-24 18:19:26 -0500  Oops -- fix to make this actually run. No longer create jabber.log if debug mode is off  r15872@phanatique:  nelhage | 2006-12-24 18:22:20 -0500  Missed one more change

--- perl/modules/jabber.pl
+++ perl/modules/jabber.pl
@@ -38,12 +38,12 @@ sub addConnection {
     my $self = shift;
     my $jidStr = shift;
 
-    my $client = Net::Jabber::Client->new(
-        owl::getvar('debug') eq 'on'
-          ? (debuglevel =>  ? 1 : 0,
-             debugfile => 'jabber.log')
-          : ()
-         );
+    my %args = ();
+    if(owl::getvar('debug') eq 'on') {
+        $args{debuglevel} = 1;
+        $args{debugfile} = 'jabber.log';
+    }
+    my $client = Net::Jabber::Client->new(%args);
 
     $self->{Client}->{$jidStr} = $client;
     $self->{Roster}->{$jidStr} = $client->Roster();
@@ -362,7 +362,7 @@ sub do_login {
                 "Error in connect: " . join( " ", @result ) );
         }
             else {
-                ${ $conn->getRosterFromJidStr($jidStr) }->fetch();
+                $conn->getRosterFromJidStr($jidStr)->fetch();
                 $client->PresenceSend( priority => 1 );
                 queue_admin_msg("Connected to jabber as $jidStr");
             }
