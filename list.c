ef6e2d15fe8333de3509f94e57d8d57b3b963292
Nelson Elhage <nelhage@mit.edu>
2007-02-27 23:37:40 -0500

    Refactoring list, to include a generic insert method

--- list.c
+++ list.c
@@ -21,46 +21,49 @@ int owl_list_get_size(owl_list *l)
   return(l->size);
 }
 
+
+void owl_list_grow(owl_list *l, int n) /*noproto*/
+{
+  void *ptr;
+
+  if ((l->size+n) > (l->avail/GROWAT)) {
+    ptr=owl_realloc(l->list, l->avail*GROWBY*sizeof(void *));
+    if (ptr==NULL) abort();
+    l->list=ptr;
+    l->avail=l->avail*GROWBY;
+  }
+
+}
+
 void *owl_list_get_element(owl_list *l, int n)
 {
   if (n>l->size-1) return(NULL);
   return(l->list[n]);
 }
 
-int owl_list_append_element(owl_list *l, void *element)
+int owl_list_insert_element(owl_list *l, int at, void *element)
 {
-  void *ptr;
-  
-  if ((l->size+1) > (l->avail/GROWAT)) {
-    ptr=owl_realloc(l->list, l->avail*GROWBY*sizeof(void *));
-    if (ptr==NULL) return(-1);
-    l->list=ptr;
-    l->avail=l->avail*GROWBY;
+  int i;
+  if(at < 0 || at > l->size) return -1;
+  owl_list_grow(l, 1);
+
+  for (i=l->size; i>at; i--) {
+    l->list[i]=l->list[i-1];
   }
 
-  l->list[l->size]=element;
+  l->list[at] = element;
   l->size++;
   return(0);
 }
 
+int owl_list_append_element(owl_list *l, void *element)
+{
+  return owl_list_insert_element(l, l->size, element);
+}
+
 int owl_list_prepend_element(owl_list *l, void *element)
 {
-  void *ptr;
-  int i;
- 
-  if ((l->size+1) > (l->avail/GROWAT)) {
-    ptr=owl_realloc(l->list, l->avail*GROWBY*sizeof(void *));
-    if (ptr==NULL) return(-1);
-    l->list=ptr;
-    l->avail=l->avail*GROWBY;
-  }
-
-  for (i=l->size; i>0; i--) {
-    l->list[i]=l->list[i-1];
-  }
-  l->list[0]=element;
-  l->size++;
-  return(0);
+  return owl_list_insert_element(l, 0, element);
 }
 
 int owl_list_remove_element(owl_list *l, int n)
