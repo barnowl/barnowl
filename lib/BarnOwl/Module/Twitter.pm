d1bb4f3adc67b84049fa2fc879c8f1c1a7136172
nelhage@mit.edu <nelhage@mit.edu>
2008-03-12 21:20:00 -0400

    Wildcard support

--- lib/BarnOwl/Module/Twitter.pm
+++ lib/BarnOwl/Module/Twitter.pm
@@ -34,20 +34,31 @@ my $opcode   = "twitter";
 # This is a workaround for http://barnowl.mit.edu/trac/ticket/44
 # Which was fixed in svn r819
 if((BarnOwl::getvar('twitter:class')||'') eq '') {
+    my $desc = <<'END_DESC';
+BarnOwl::Module::Twitter will watch for authentic zephyrs to
+-c $twitter:class -i $twitter:instance -O $twitter:opcode
+from your sender and mirror them to Twitter.
+
+A value of '*' in any of these fields acts a wildcard, accepting
+messages with any value of that field.
+END_DESC
     BarnOwl::new_variable_string('twitter:class',
                                {
                                    default => $class,
-                                   summary => 'Class to watch for Twitter messages'
+                                   summary => 'Class to watch for Twitter messages',
+                                   description => $desc
                                   });
     BarnOwl::new_variable_string('twitter:instance',
                                {
                                    default => $instance,
-                                   summary => 'Instance on twitter:class to watch for Twitter messages'
+                                   summary => 'Instance on twitter:class to watch for Twitter messages.',
+                                   description => $desc
                                   });
     BarnOwl::new_variable_string('twitter:opcode',
                                {
                                    default => $opcode,
-                                   summary => 'Opcode for zephyrs that will be sent as twitter updates'
+                                   summary => 'Opcode for zephyrs that will be sent as twitter updates',
+                                   description => $desc
                                   });
 }
 
@@ -70,13 +81,19 @@ if(!defined($twitter->verify_credentials())) {
     fail("Invalid twitter credentials");
 }
 
+sub match {
+    my $val = shift;
+    my $pat = shift;
+    return $pat eq "*" || ($val eq $pat);
+}
+
 sub handle_message {
     my $m = shift;
     ($class, $instance, $opcode) = map{BarnOwl::getvar("twitter:$_")} qw(class instance opcode);
     if($m->sender eq $user
-       && $m->class eq $class
-       && $m->instance eq $instance
-       && $m->opcode eq $opcode
+       && match($m->class, $class)
+       && match($m->instance, $instance)
+       && match($m->opcode, $opcode)
        && $m->auth eq 'YES') {
         twitter($m->body);
     }
