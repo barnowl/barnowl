c19470517d9c6826000d35581121f509de4dca32
Nelson Elhage <nelhage@mit.edu>
2008-09-16 01:09:13 +0000

    1.0.2.1

--- owl.h
+++ owl.h
@@ -55,7 +55,7 @@ static const char owl_h_fileIdent[] = "$Id$";
 #endif
 
 #ifndef OWL_VERSION_STRING
-#define OWL_VERSION_STRING "1.0.2"
+#define OWL_VERSION_STRING "1.0.2.1"
 #endif
 
 /* Feature that is being tested to redirect stderr through a pipe. 
