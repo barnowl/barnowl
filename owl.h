6fdef660f762d2eddb7d7ec722cd15a3255a8dca
James M. Kretchmar <kretch@mit.edu>
2009-03-29 23:19:53 +0000

    Call this 2.2.0

--- owl.h
+++ owl.h
@@ -46,7 +46,7 @@
 static const char owl_h_fileIdent[] = "$Id$";
 
 #define OWL_VERSION         2.2.0
-#define OWL_VERSION_STRING "2.2.0-pre2"
+#define OWL_VERSION_STRING "2.2.0"
 
 /* Feature that is being tested to redirect stderr through a pipe. 
  * There may still be some portability problems with this. */
