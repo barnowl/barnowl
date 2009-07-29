d4ecc78a21e73cd128edd0597e1e60ccdcf205b9
David Benjamin <davidben@mit.edu>
2009-07-28 22:36:01 -0400

    Complete color names for filters
    This will likely have to be changed later, but for now, it's something.
    
    Signed-off-by: David Benjamin <davidben@mit.edu>

--- perl/lib/BarnOwl/Complete/Client.pm
+++ perl/lib/BarnOwl/Complete/Client.pm
@@ -7,7 +7,18 @@ package BarnOwl::Complete::Client;
 
 use BarnOwl::Completion::Util qw(complete_flags);
 
+my @all_colors = qw(default
+                    black
+                    blue
+                    cyan
+                    green
+                    magenta
+                    red
+                    white
+                    yellow);
+
 sub complete_command { return sort @BarnOwl::all_commands; }
+sub complete_color { return @all_colors; }
 
 sub complete_help {
     my $ctx = shift;
@@ -19,6 +30,20 @@ sub complete_help {
        );
 }
 
-BarnOwl::Completion::register_completer(help      => \&complete_help);
+sub complete_filter {
+    my $ctx = shift;
+    return complete_flags($ctx,
+        [qw()],
+        {
+           "-c" => \&complete_color,
+           "-b" => \&complete_color,
+        },
+        undef # TODO: make this get list of filters for the first argument
+              # then maybe some magic like completing keywords instance and other shiny
+        );
+}
+
+BarnOwl::Completion::register_completer(help    => \&complete_help);
+BarnOwl::Completion::register_completer(filter  => \&complete_filter);
 
 1;
