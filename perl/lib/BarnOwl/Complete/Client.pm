1610e5bf4c18badd991de18fa86d192690f45916
Nelson Elhage <nelhage@mit.edu>
2009-07-29 22:19:03 -0400

    Implement completion for the 'show' command.

--- perl/lib/BarnOwl/Complete/Client.pm
+++ perl/lib/BarnOwl/Complete/Client.pm
@@ -17,14 +17,38 @@ my @all_colors = qw(default
                     white
                     yellow);
 
+my %show = (
+    information => undef,
+    colors      => undef,
+    commands    => undef,
+    command     => \&complete_command,
+    filters     => undef,
+    filter      => \&complete_filter_name,
+    license     => undef,
+    quickstart  => undef,
+    startup     => undef,
+    status      => undef,
+    styles      => undef,
+    subscriptions       => undef,
+    subs        => undef,
+    terminal    => undef,
+    variables   => undef,
+    variable    => \&complete_variable,
+    version     => undef,
+    view        => undef,
+    zpunts      => undef,
+   );
+
 sub complete_command { return sort @BarnOwl::all_commands; }
 sub complete_color { return @all_colors; }
+sub complete_filter_name { return @{BarnOwl::all_filters()}; }
+sub complete_variable {}        # stub
 
 sub complete_filter_args {
     my $ctx = shift;
     my $arg = shift;
     return if $arg;
-    return @{BarnOwl::all_filters()};
+    return complete_filter_name();
 }
 
 sub complete_help {
@@ -35,6 +59,18 @@ sub complete_help {
         },
         \&complete_command
        );
+}
+
+sub complete_show {
+    my $ctx = shift;
+    if($ctx->word == 1) {
+        return keys %show;
+    } elsif ($ctx->word == 2) {
+        my $cmd = $show{$ctx->words->[1]};
+        if($cmd) {
+            return $cmd->($ctx);
+        }
+    }
 }
 
 sub complete_filter {
@@ -51,5 +87,6 @@ sub complete_filter {
 
 BarnOwl::Completion::register_completer(help    => \&complete_help);
 BarnOwl::Completion::register_completer(filter  => \&complete_filter);
+BarnOwl::Completion::register_completer(show    => \&complete_show);
 
 1;
