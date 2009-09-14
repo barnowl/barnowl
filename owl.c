4e4847c43520c792fa6e17596790361ca5db61a9
Nelson Elhage <nelhage@mit.edu>
2009-09-14 09:16:16 -0400

    Don't include owl.c when running codelist.

--- owl.c
+++ owl.c
@@ -36,7 +36,7 @@ int stderr_replace(void);
 
 owl_global g;
 
-typedef struct _owl_options {           /* noproto */
+typedef struct _owl_options {
   bool load_initial_subs;
   char *configfile;
   char *tty;
@@ -45,8 +45,22 @@ typedef struct _owl_options {           /* noproto */
   bool rm_debug;
 } owl_options;
 
+void usage()
+{
+  fprintf(stderr, "Barnowl version %s\n", OWL_VERSION_STRING);
+  fprintf(stderr, "Usage: barnowl [-n] [-d] [-D] [-v] [-h] [-c <configfile>] [-s <confdir>] [-t <ttyname>]\n");
+  fprintf(stderr, "  -n,--no-subs        don't load zephyr subscriptions\n");
+  fprintf(stderr, "  -d,--debug          enable debugging\n");
+  fprintf(stderr, "  -D,--remove-debug   enable debugging and delete previous debug file\n");
+  fprintf(stderr, "  -v,--version        print the Barnowl version number and exit\n");
+  fprintf(stderr, "  -h,--help           print this help message\n");
+  fprintf(stderr, "  -c,--config-file    specify an alternate config file\n");
+  fprintf(stderr, "  -s,--config-dir     specify an alternate config dir (default ~/.owl)\n");
+  fprintf(stderr, "  -t,--tty            set the tty name\n");
+}
+
 /* TODO: free owl_options after init is done? */
-void owl_parse_options(int argc, char *argv[], owl_options *opts) /* noproto */ {
+void owl_parse_options(int argc, char *argv[], owl_options *opts) {
   static const struct option long_options[] = {
     { "no-subs",         0, 0, 'n' },
     { "config-file",     1, 0, 'c' },
@@ -92,26 +106,7 @@ void owl_parse_options(int argc, char *argv[], owl_options *opts) /* noproto */ 
   }
 }
 
-void owl_register_signal_handlers(void) /* noproto */ {
-  struct sigaction sigact;
-
-  /* signal handler */
-  /*sigact.sa_handler=sig_handler;*/
-  sigact.sa_sigaction=sig_handler;
-  sigemptyset(&sigact.sa_mask);
-  sigact.sa_flags=SA_SIGINFO;
-  sigaction(SIGWINCH, &sigact, NULL);
-  sigaction(SIGALRM, &sigact, NULL);
-  sigaction(SIGPIPE, &sigact, NULL);
-  sigaction(SIGTERM, &sigact, NULL);
-  sigaction(SIGHUP, &sigact, NULL);
-
-  sigact.sa_sigaction=sigint_handler;
-  sigaction(SIGINT, &sigact, NULL);
-
-}
-
-void owl_start_color(void) /* noproto */ {
+void owl_start_color(void) {
   start_color();
 #ifdef HAVE_USE_DEFAULT_COLORS
   use_default_colors();
@@ -134,7 +129,7 @@ void owl_start_color(void) /* noproto */ {
   }
 }
 
-void owl_start_curses(void) /* noproto */ {
+void owl_start_curses(void) {
   struct termios tio;
   /* save initial terminal settings */
   tcgetattr(0, owl_global_get_startup_tio(&g));
@@ -153,7 +148,7 @@ void owl_start_curses(void) /* noproto */ {
   owl_start_color();
 }
 
-void owl_setup_default_filters(void) /* noproto */ {
+void owl_setup_default_filters(void) {
   owl_filter *f;
 
   owl_function_debugmsg("startup: creating default filters");
@@ -206,6 +201,260 @@ void owl_setup_default_filters(void) /* noproto */ {
   owl_filter_init_fromstring(f, "all", "true");
   owl_list_append_element(owl_global_get_filterlist(&g), f);
 }
+
+/*
+ * Process a new message passed to us on the message queue from some
+ * protocol. This includes adding it to the message list, updating the
+ * view and scrolling if appropriate, logging it, and so on.
+ *
+ * Either a pointer is kept to the message internally, or it is freed
+ * if unneeded. The caller no longer ``owns'' the message's memory.
+ *
+ * Returns 1 if the message was added to the message list, and 0 if it
+ * was ignored due to user settings or otherwise.
+ */
+int owl_process_message(owl_message *m) {
+  const owl_filter *f;
+  /* if this message it on the puntlist, nuke it and continue */
+  if (owl_global_message_is_puntable(&g, m)) {
+    owl_message_free(m);
+    return 0;
+  }
+
+  /*  login or logout that should be ignored? */
+  if (owl_global_is_ignorelogins(&g)
+      && owl_message_is_loginout(m)) {
+    owl_message_free(m);
+    return 0;
+  }
+
+  if (!owl_global_is_displayoutgoing(&g)
+      && owl_message_is_direction_out(m)) {
+    owl_message_free(m);
+    return 0;
+  }
+
+  /* add it to the global list */
+  owl_messagelist_append_element(owl_global_get_msglist(&g), m);
+  /* add it to any necessary views; right now there's only the current view */
+  owl_view_consider_message(owl_global_get_current_view(&g), m);
+
+  if(owl_message_is_direction_in(m)) {
+    /* let perl know about it*/
+    owl_perlconfig_getmsg(m, NULL);
+
+    /* do we need to autoreply? */
+    if (owl_global_is_zaway(&g) && !owl_message_get_attribute_value(m, "isauto")) {
+      if (owl_message_is_type_zephyr(m)) {
+        owl_zephyr_zaway(m);
+      } else if (owl_message_is_type_aim(m)) {
+        if (owl_message_is_private(m)) {
+          owl_function_send_aimawymsg(owl_message_get_sender(m), owl_global_get_zaway_msg(&g));
+        }
+      }
+    }
+
+    /* ring the bell if it's a personal */
+    if (!strcmp(owl_global_get_personalbell(&g), "on")) {
+      if (!owl_message_is_loginout(m) &&
+          !owl_message_is_mail(m) &&
+          owl_message_is_personal(m)) {
+        owl_function_beep();
+      }
+    } else if (!strcmp(owl_global_get_personalbell(&g), "off")) {
+      /* do nothing */
+    } else {
+      f=owl_global_get_filter(&g, owl_global_get_personalbell(&g));
+      if (f && owl_filter_message_match(f, m)) {
+        owl_function_beep();
+      }
+    }
+
+    /* if it matches the alert filter, do the alert action */
+    f=owl_global_get_filter(&g, owl_global_get_alert_filter(&g));
+    if (f && owl_filter_message_match(f, m)) {
+      owl_function_command(owl_global_get_alert_action(&g));
+    }
+
+    /* if it's a zephyr login or logout, update the zbuddylist */
+    if (owl_message_is_type_zephyr(m) && owl_message_is_loginout(m)) {
+      if (owl_message_is_login(m)) {
+        owl_zbuddylist_adduser(owl_global_get_zephyr_buddylist(&g), owl_message_get_sender(m));
+      } else if (owl_message_is_logout(m)) {
+        owl_zbuddylist_deluser(owl_global_get_zephyr_buddylist(&g), owl_message_get_sender(m));
+      } else {
+        owl_function_error("Internal error: received login notice that is neither login nor logout");
+      }
+    }
+  }
+
+  /* let perl know about it */
+  owl_perlconfig_newmsg(m, NULL);
+  /* log the message if we need to */
+  owl_log_message(m);
+
+  return 1;
+}
+
+void owl_process_input(owl_dispatch *d)
+{
+  owl_input j;
+  WINDOW *typwin;
+
+  typwin = owl_global_get_curs_typwin(&g);
+
+  while (1) {
+    j.ch = wgetch(typwin);
+    if (j.ch == ERR) return;
+
+    j.uch = '\0';
+    if (j.ch >= KEY_MIN && j.ch <= KEY_MAX) {
+      /* This is a curses control character. */
+    }
+    else if (j.ch > 0x7f && j.ch < 0xfe) {
+      /* Pull in a full utf-8 character. */
+      int bytes, i;
+      char utf8buf[7];
+      memset(utf8buf, '\0', 7);
+      
+      utf8buf[0] = j.ch;
+      
+      if ((j.ch & 0xc0) && (~j.ch & 0x20)) bytes = 2;
+      else if ((j.ch & 0xe0) && (~j.ch & 0x10)) bytes = 3;
+      else if ((j.ch & 0xf0) && (~j.ch & 0x08)) bytes = 4;
+      else if ((j.ch & 0xf8) && (~j.ch & 0x04)) bytes = 5;
+      else if ((j.ch & 0xfc) && (~j.ch & 0x02)) bytes = 6;
+      else bytes = 1;
+      
+      for (i = 1; i < bytes; i++) {
+        int tmp =  wgetch(typwin);
+        /* If what we got was not a byte, or not a continuation byte */
+        if (tmp > 0xff || !(tmp & 0x80 && ~tmp & 0x40)) {
+          /* ill-formed UTF-8 code unit subsequence, put back the
+             char we just got. */
+          ungetch(tmp);
+          j.ch = ERR;
+          break;
+        }
+        utf8buf[i] = tmp;
+      }
+      
+      if (j.ch != ERR) {
+        if (g_utf8_validate(utf8buf, -1, NULL)) {
+          j.uch = g_utf8_get_char(utf8buf);
+        }
+        else {
+          j.ch = ERR;
+        }
+      }
+    }
+    else if (j.ch <= 0x7f) {
+      j.uch = j.ch;
+    }
+
+    owl_process_input_char(j);
+  }
+}
+
+void sig_handler(int sig, siginfo_t *si, void *data)
+{
+  if (sig==SIGWINCH) {
+    /* we can't inturrupt a malloc here, so it just sets a flag
+     * schedulding a resize for later
+     */
+    owl_function_resize();
+  } else if (sig==SIGPIPE || sig==SIGCHLD) {
+    /* Set a flag and some info that we got the sigpipe
+     * so we can record that we got it and why... */
+    owl_global_set_errsignal(&g, sig, si);
+  } else if (sig==SIGTERM || sig==SIGHUP) {
+    owl_function_quit();
+  }
+}
+
+void sigint_handler(int sig, siginfo_t *si, void *data)
+{
+  owl_global_set_interrupted(&g);
+}
+
+void owl_register_signal_handlers(void) {
+  struct sigaction sigact;
+
+  /* signal handler */
+  /*sigact.sa_handler=sig_handler;*/
+  sigact.sa_sigaction=sig_handler;
+  sigemptyset(&sigact.sa_mask);
+  sigact.sa_flags=SA_SIGINFO;
+  sigaction(SIGWINCH, &sigact, NULL);
+  sigaction(SIGALRM, &sigact, NULL);
+  sigaction(SIGPIPE, &sigact, NULL);
+  sigaction(SIGTERM, &sigact, NULL);
+  sigaction(SIGHUP, &sigact, NULL);
+
+  sigact.sa_sigaction=sigint_handler;
+  sigaction(SIGINT, &sigact, NULL);
+
+}
+
+#if OWL_STDERR_REDIR
+
+/* Replaces stderr with a pipe so that we can read from it. 
+ * Returns the fd of the pipe from which stderr can be read. */
+int stderr_replace(void)
+{
+  int pipefds[2];
+  if (0 != pipe(pipefds)) {
+    perror("pipe");
+    owl_function_debugmsg("stderr_replace: pipe FAILED\n");
+    return -1;
+  }
+    owl_function_debugmsg("stderr_replace: pipe: %d,%d\n", pipefds[0], pipefds[1]);
+  if (-1 == dup2(pipefds[1], 2 /*stderr*/)) {
+    owl_function_debugmsg("stderr_replace: dup2 FAILED (%s)\n", strerror(errno));
+    perror("dup2");
+    return -1;
+  }
+  return pipefds[0];
+}
+
+/* Sends stderr (read from rfd) messages to the error console */
+void stderr_redirect_handler(owl_dispatch *d)
+{
+  int navail, bread;
+  char buf[4096];
+  int rfd = d->fd;
+  char *err;
+
+  if (rfd<0) return;
+  if (-1 == ioctl(rfd, FIONREAD, &navail)) {
+    return;
+  }
+  /*owl_function_debugmsg("stderr_redirect: navail = %d\n", navail);*/
+  if (navail <= 0) return;
+  if (navail > sizeof(buf)-1) {
+    navail = sizeof(buf)-1;
+  }
+  bread = read(rfd, buf, navail);
+  if (buf[navail-1] != '\0') {
+    buf[navail] = '\0';
+  }
+
+  err = owl_sprintf("[stderr]\n%s", buf);
+
+  owl_function_log_err(err);
+  owl_free(err);
+}
+
+#endif /* OWL_STDERR_REDIR */
+
+void owl_zephyr_buddycheck_timer(owl_timer *t, void *data)
+{
+  if (owl_global_is_pseudologins(&g)) {
+    owl_function_debugmsg("Doing zephyr buddy check");
+    owl_function_zephyr_buddy_check(1);
+  }
+}
+
 
 int main(int argc, char **argv, char **env)
 {
@@ -480,253 +729,5 @@ int main(int argc, char **argv, char **env)
       }
     }
 
-  }
-}
-
-/*
- * Process a new message passed to us on the message queue from some
- * protocol. This includes adding it to the message list, updating the
- * view and scrolling if appropriate, logging it, and so on.
- *
- * Either a pointer is kept to the message internally, or it is freed
- * if unneeded. The caller no longer ``owns'' the message's memory.
- *
- * Returns 1 if the message was added to the message list, and 0 if it
- * was ignored due to user settings or otherwise.
- */
-int owl_process_message(owl_message *m) {
-  const owl_filter *f;
-  /* if this message it on the puntlist, nuke it and continue */
-  if (owl_global_message_is_puntable(&g, m)) {
-    owl_message_free(m);
-    return 0;
-  }
-
-  /*  login or logout that should be ignored? */
-  if (owl_global_is_ignorelogins(&g)
-      && owl_message_is_loginout(m)) {
-    owl_message_free(m);
-    return 0;
-  }
-
-  if (!owl_global_is_displayoutgoing(&g)
-      && owl_message_is_direction_out(m)) {
-    owl_message_free(m);
-    return 0;
-  }
-
-  /* add it to the global list */
-  owl_messagelist_append_element(owl_global_get_msglist(&g), m);
-  /* add it to any necessary views; right now there's only the current view */
-  owl_view_consider_message(owl_global_get_current_view(&g), m);
-
-  if(owl_message_is_direction_in(m)) {
-    /* let perl know about it*/
-    owl_perlconfig_getmsg(m, NULL);
-
-    /* do we need to autoreply? */
-    if (owl_global_is_zaway(&g) && !owl_message_get_attribute_value(m, "isauto")) {
-      if (owl_message_is_type_zephyr(m)) {
-        owl_zephyr_zaway(m);
-      } else if (owl_message_is_type_aim(m)) {
-        if (owl_message_is_private(m)) {
-          owl_function_send_aimawymsg(owl_message_get_sender(m), owl_global_get_zaway_msg(&g));
-        }
-      }
-    }
-
-    /* ring the bell if it's a personal */
-    if (!strcmp(owl_global_get_personalbell(&g), "on")) {
-      if (!owl_message_is_loginout(m) &&
-          !owl_message_is_mail(m) &&
-          owl_message_is_personal(m)) {
-        owl_function_beep();
-      }
-    } else if (!strcmp(owl_global_get_personalbell(&g), "off")) {
-      /* do nothing */
-    } else {
-      f=owl_global_get_filter(&g, owl_global_get_personalbell(&g));
-      if (f && owl_filter_message_match(f, m)) {
-        owl_function_beep();
-      }
-    }
-
-    /* if it matches the alert filter, do the alert action */
-    f=owl_global_get_filter(&g, owl_global_get_alert_filter(&g));
-    if (f && owl_filter_message_match(f, m)) {
-      owl_function_command(owl_global_get_alert_action(&g));
-    }
-
-    /* if it's a zephyr login or logout, update the zbuddylist */
-    if (owl_message_is_type_zephyr(m) && owl_message_is_loginout(m)) {
-      if (owl_message_is_login(m)) {
-        owl_zbuddylist_adduser(owl_global_get_zephyr_buddylist(&g), owl_message_get_sender(m));
-      } else if (owl_message_is_logout(m)) {
-        owl_zbuddylist_deluser(owl_global_get_zephyr_buddylist(&g), owl_message_get_sender(m));
-      } else {
-        owl_function_error("Internal error: received login notice that is neither login nor logout");
-      }
-    }
-  }
-
-  /* let perl know about it */
-  owl_perlconfig_newmsg(m, NULL);
-  /* log the message if we need to */
-  owl_log_message(m);
-
-  return 1;
-}
-
-void owl_process_input(owl_dispatch *d)
-{
-  owl_input j;
-  WINDOW *typwin;
-
-  typwin = owl_global_get_curs_typwin(&g);
-
-  while (1) {
-    j.ch = wgetch(typwin);
-    if (j.ch == ERR) return;
-
-    j.uch = '\0';
-    if (j.ch >= KEY_MIN && j.ch <= KEY_MAX) {
-      /* This is a curses control character. */
-    }
-    else if (j.ch > 0x7f && j.ch < 0xfe) {
-      /* Pull in a full utf-8 character. */
-      int bytes, i;
-      char utf8buf[7];
-      memset(utf8buf, '\0', 7);
-      
-      utf8buf[0] = j.ch;
-      
-      if ((j.ch & 0xc0) && (~j.ch & 0x20)) bytes = 2;
-      else if ((j.ch & 0xe0) && (~j.ch & 0x10)) bytes = 3;
-      else if ((j.ch & 0xf0) && (~j.ch & 0x08)) bytes = 4;
-      else if ((j.ch & 0xf8) && (~j.ch & 0x04)) bytes = 5;
-      else if ((j.ch & 0xfc) && (~j.ch & 0x02)) bytes = 6;
-      else bytes = 1;
-      
-      for (i = 1; i < bytes; i++) {
-        int tmp =  wgetch(typwin);
-        /* If what we got was not a byte, or not a continuation byte */
-        if (tmp > 0xff || !(tmp & 0x80 && ~tmp & 0x40)) {
-          /* ill-formed UTF-8 code unit subsequence, put back the
-             char we just got. */
-          ungetch(tmp);
-          j.ch = ERR;
-          break;
-        }
-        utf8buf[i] = tmp;
-      }
-      
-      if (j.ch != ERR) {
-        if (g_utf8_validate(utf8buf, -1, NULL)) {
-          j.uch = g_utf8_get_char(utf8buf);
-        }
-        else {
-          j.ch = ERR;
-        }
-      }
-    }
-    else if (j.ch <= 0x7f) {
-      j.uch = j.ch;
-    }
-
-    owl_process_input_char(j);
-  }
-}
-
-void sig_handler(int sig, siginfo_t *si, void *data)
-{
-  if (sig==SIGWINCH) {
-    /* we can't inturrupt a malloc here, so it just sets a flag
-     * schedulding a resize for later
-     */
-    owl_function_resize();
-  } else if (sig==SIGPIPE || sig==SIGCHLD) {
-    /* Set a flag and some info that we got the sigpipe
-     * so we can record that we got it and why... */
-    owl_global_set_errsignal(&g, sig, si);
-  } else if (sig==SIGTERM || sig==SIGHUP) {
-    owl_function_quit();
-  }
-}
-
-void sigint_handler(int sig, siginfo_t *si, void *data)
-{
-  owl_global_set_interrupted(&g);
-}
-
-void usage()
-{
-  fprintf(stderr, "Barnowl version %s\n", OWL_VERSION_STRING);
-  fprintf(stderr, "Usage: barnowl [-n] [-d] [-D] [-v] [-h] [-c <configfile>] [-s <confdir>] [-t <ttyname>]\n");
-  fprintf(stderr, "  -n,--no-subs        don't load zephyr subscriptions\n");
-  fprintf(stderr, "  -d,--debug          enable debugging\n");
-  fprintf(stderr, "  -D,--remove-debug   enable debugging and delete previous debug file\n");
-  fprintf(stderr, "  -v,--version        print the Barnowl version number and exit\n");
-  fprintf(stderr, "  -h,--help           print this help message\n");
-  fprintf(stderr, "  -c,--config-file    specify an alternate config file\n");
-  fprintf(stderr, "  -s,--config-dir     specify an alternate config dir (default ~/.owl)\n");
-  fprintf(stderr, "  -t,--tty            set the tty name\n");
-}
-
-#if OWL_STDERR_REDIR
-
-/* Replaces stderr with a pipe so that we can read from it. 
- * Returns the fd of the pipe from which stderr can be read. */
-int stderr_replace(void)
-{
-  int pipefds[2];
-  if (0 != pipe(pipefds)) {
-    perror("pipe");
-    owl_function_debugmsg("stderr_replace: pipe FAILED\n");
-    return -1;
-  }
-    owl_function_debugmsg("stderr_replace: pipe: %d,%d\n", pipefds[0], pipefds[1]);
-  if (-1 == dup2(pipefds[1], 2 /*stderr*/)) {
-    owl_function_debugmsg("stderr_replace: dup2 FAILED (%s)\n", strerror(errno));
-    perror("dup2");
-    return -1;
-  }
-  return pipefds[0];
-}
-
-/* Sends stderr (read from rfd) messages to the error console */
-void stderr_redirect_handler(owl_dispatch *d)
-{
-  int navail, bread;
-  char buf[4096];
-  int rfd = d->fd;
-  char *err;
-
-  if (rfd<0) return;
-  if (-1 == ioctl(rfd, FIONREAD, &navail)) {
-    return;
-  }
-  /*owl_function_debugmsg("stderr_redirect: navail = %d\n", navail);*/
-  if (navail <= 0) return;
-  if (navail > sizeof(buf)-1) {
-    navail = sizeof(buf)-1;
-  }
-  bread = read(rfd, buf, navail);
-  if (buf[navail-1] != '\0') {
-    buf[navail] = '\0';
-  }
-
-  err = owl_sprintf("[stderr]\n%s", buf);
-
-  owl_function_log_err(err);
-  owl_free(err);
-}
-
-#endif /* OWL_STDERR_REDIR */
-
-void owl_zephyr_buddycheck_timer(owl_timer *t, void *data)
-{
-  if (owl_global_is_pseudologins(&g)) {
-    owl_function_debugmsg("Doing zephyr buddy check");
-    owl_function_zephyr_buddy_check(1);
   }
 }
