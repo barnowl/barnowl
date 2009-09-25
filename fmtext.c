7b4d90e2eb25780ff26b3655b57676b3272cbc5b
David Benjamin <davidben@mit.edu>
2009-09-25 19:43:51 -0400

    Handle missing newline in owl_fmtext_truncate_cols
    Fixes #51. Also document some behavior of the function and add a test.
    
    Signed-off-by: David Benjamin <davidben@mit.edu>

--- fmtext.c
+++ fmtext.c
@@ -417,11 +417,14 @@ int owl_fmtext_truncate_lines(const owl_fmtext *in, int aline, int lines, owl_fm
 
 /* Truncate the message so that each line begins at column 'acol' and
  * ends at 'bcol' or sooner.  The first column is number 0.  The new
- * message is placed in 'out'.  The message is * expected to end in a
- * new line for now. NOTE: This needs to be modified to deal with
- * backing up if we find a SPACING COMBINING MARK at the end of a
- * line. If that happens, we should back up to the last non-mark
- * character and stop there.
+ * message is placed in 'out'.
+ *
+ * NOTE: This needs to be modified to deal with backing up if we find
+ * a SPACING * COMBINING MARK at the end of a line. If that happens,
+ * we should back up to * the last non-mark character and stop there.
+ *
+ * NOTE: If a line ends at bcol, we omit the newline. This is so printing
+ * to ncurses works.
  */
 void owl_fmtext_truncate_cols(const owl_fmtext *in, int acol, int bcol, owl_fmtext *out)
 {
@@ -438,8 +441,9 @@ void owl_fmtext_truncate_cols(const owl_fmtext *in, int acol, int bcol, owl_fmte
   while (ptr_s <= last) {
     ptr_e=strchr(ptr_s, '\n');
     if (!ptr_e) {
-      /* but this shouldn't happen if we end in a \n */
-      break;
+      /* set it to the NULL at the end of the string, to handle a line that is
+       * not newline-terminated */
+      ptr_e = last+1;
     }
     
     if (ptr_e == ptr_s) {
@@ -494,7 +498,11 @@ void owl_fmtext_truncate_cols(const owl_fmtext *in, int acol, int bcol, owl_fmte
     else {
       owl_fmtext_append_normal(out, "\n");
     }
-    ptr_s = g_utf8_next_char(ptr_e);
+    /* Be careful if ptr_e is pointing to the NULL if we do not end with a
+     * newline. We don't really need this check, but pointing ptr_s past the
+     * NULL byte is just asking for problems later.
+     */
+    ptr_s = (ptr_e <= last) ? g_utf8_next_char(ptr_e) : ptr_e;
   }
 }
 
