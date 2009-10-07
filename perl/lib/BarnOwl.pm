barnowl/barnowl

commit de3f6412cb0dc1b4e1044541558395c4c1fd807c

Perl callout for zsig computation + move default to perl
Barnowl::default_zephyr_signature works out what your zsig
should be.

New owl variable zsigfunc, which contains a perl expression
that is evaluated for the zsig.  Defaults to the above.

This is much more pleasant code in perl than in C.

perl/lib/BarnOwl.pm
@@ -426,6 +426,29 @@ sub time_format
     redisplay();
 }
 
+=head3 default_zephyr_signature
+
+Compute the default zephyr signature.
+
+=cut
+
+sub default_zephyr_signature
+{
+  if (my $zsig = getvar('zsig')) {
+    return $zsig;
+  }
+  if (my $zsigproc = getvar('zsigproc')) {
+    return `$zsigproc`;
+  }
+  my $zwrite_signature = get_zephyr_variable('zwrite-signature');
+  if (defined($zwrite_signature)) {
+    return $zwrite_signature;
+  }
+  my $name = ((getpwuid($<))[6]);
+  $name =~ s/,.*//;
+  return $name;
+}
+
 # Stub for owl::startup / BarnOwl::startup, so it isn't bound to the
 # startup command. This may be redefined in a user's configfile.
 sub startup

variable.c
@@ -208,6 +208,12 @@ static owl_variable variables_to_init[] = {
          "principal. Note that customizing the sender name will\n"
          "cause your zephyrs to be sent unauthenticated."),
 
+  OWLVAR_STRING( "zsigfunc" /* %OwlVarStub */, "BarnOwl::default_zephyr_signature()",
+		 "zsig perl function",
+		 "Called every time you start a zephyrgram without an\n"
+		 "explicit zsig.  The default setting implements the policy\n"
+		 "descripted in the documentation for the 'zsig' variable.\n"),
+
   OWLVAR_STRING( "zsig" /* %OwlVarStub */, "",
 	         "zephyr signature",
 		 "The zsig to get on outgoing messages. If this variable is\n"

zwrite.c
@@ -153,55 +153,11 @@ int owl_zwrite_create_from_line(owl_zwrite *z, const char *line)
 
 void owl_zwrite_populate_zsig(owl_zwrite *z)
 {
-  const char *zsigproc, *zsigowlvar, *zsigzvar;
-  char *ptr;
-  struct passwd *pw;
-
   /* get a zsig, if not given */
-  if (z->zsig==NULL) {
-    zsigproc = owl_global_get_zsigproc(&g);
-    zsigowlvar = owl_global_get_zsig(&g);
-    zsigzvar = owl_zephyr_get_variable("zwrite-signature");
+  if (z->zsig != NULL)
+    return;
 
-    if (zsigowlvar && *zsigowlvar) {
-      z->zsig=owl_validate_utf8(zsigowlvar);
-    } else if (zsigproc && *zsigproc) {
-      FILE *file;
-      char buff[LINE], *openline;
-      
-      /* simple hack for now to nuke stderr */
-#if OWL_STDERR_REDIR
-      openline = owl_strdup(zsigproc);
-#else
-      openline = owl_sprintf("%s 2> /dev/null", zsigproc);
-#endif
-      file=popen(openline, "r");
-      owl_free(openline);
-      if (!file) {
-	if (zsigzvar && *zsigzvar) {
-	  z->zsig=owl_validate_utf8(zsigzvar);
-	}
-      } else {
-	z->zsig=owl_malloc(LINE*5);
-	strcpy(z->zsig, "");
-	while (fgets(buff, LINE, file)) { /* wrong sizing */
-	  strcat(z->zsig, buff);
-	}
-	pclose(file);
-	if (z->zsig[0] != '\0' && z->zsig[strlen(z->zsig) - 1] == '\n') {
-	  z->zsig[strlen(z->zsig)-1]='\0';
-	}
-      }
-    } else if (zsigzvar) {
-      z->zsig=owl_validate_utf8(zsigzvar);
-    } else if (((pw=getpwuid(getuid()))!=NULL) && (pw->pw_gecos)) {
-      z->zsig=owl_validate_utf8(pw->pw_gecos);
-      ptr=strchr(z->zsig, ',');
-      if (ptr) {
-	ptr[0]='\0';
-      }
-    }
-  }
+  z->zsig = owl_perlconfig_execute(owl_global_get_zsigfunc(&g));
 }
 
 void owl_zwrite_send_ping(const owl_zwrite *z)

commit 77c87b2b8f0028eea432fb75a14183ed321f5f1c

Strip trailing newlines from zsigs.
de3f6412cb0dc1b4e1044541558395c4c1fd807c caused a regression by not
dropping the trailing newline from the output of zsigproc.  Stripping
trailing newlines is generally a good thing, so let's do more of it.

perl/lib/BarnOwl.pm
@@ -434,19 +434,15 @@ Compute the default zephyr signature.
 
 sub default_zephyr_signature
 {
-  if (my $zsig = getvar('zsig')) {
-    return $zsig;
+  my $zsig = getvar('zsig');
+  if (!$zsig && (my $zsigproc = getvar('zsigproc'))) {
+    $zsig = `$zsigproc`;
+  } elsif (!defined($zsig = get_zephyr_variable('zwrite-signature'))) {
+    $zsig = ((getpwuid($<))[6]);
+    $zsig =~ s/,.*//;
   }
-  if (my $zsigproc = getvar('zsigproc')) {
-    return `$zsigproc`;
-  }
-  my $zwrite_signature = get_zephyr_variable('zwrite-signature');
-  if (defined($zwrite_signature)) {
-    return $zwrite_signature;
-  }
-  my $name = ((getpwuid($<))[6]);
-  $name =~ s/,.*//;
-  return $name;
+  chomp($zsig);
+  return $zsig;
 }
 
 # Stub for owl::startup / BarnOwl::startup, so it isn't bound to the