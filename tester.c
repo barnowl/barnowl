e93dd785a2e76aa0dc14ecd56fd2c61b3598bc96
David Benjamin <davidben@mit.edu>
2011-03-08 12:48:12 -0500

    Properly tear down the ncurses screen in tester.c
    There's actually a set of valgrind suppressions in ncurses, and there
    really isn't API to clean up after initscr. But since we allocate this
    SCREEN ourselves, may as well clean it up.

--- tester.c
+++ tester.c
@@ -31,6 +31,7 @@ int main(int argc, char **argv, char **env)
   FILE *wnull;
   char *perlerr;
   int status = 0;
+  SCREEN *screen;
 
   if (argc <= 1) {
     fprintf(stderr, "Usage: %s --builtin|TEST.t|-le CODE\n", argv[0]);
@@ -40,7 +41,7 @@ int main(int argc, char **argv, char **env)
   /* initialize a fake ncurses, detached from std{in,out} */
   wnull = fopen("/dev/null", "w");
   rnull = fopen("/dev/null", "r");
-  newterm("xterm", wnull, rnull);
+  screen = newterm("xterm", wnull, rnull);
   /* initialize global structures */
   owl_global_init(&g);
 
@@ -90,6 +91,7 @@ int main(int argc, char **argv, char **env)
   perl_free(owl_global_get_perlinterp(&g));
   /* probably not necessary, but tear down the screen */
   endwin();
+  delscreen(screen);
   fclose(rnull);
   fclose(wnull);
   return status;
