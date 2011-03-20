4dde585fcac5c152e22a20684548501eaf803869
Anders Kaseorg <andersk@mit.edu>
2011-03-19 23:18:24 -0400

    compat/compat.h: Add #include guards
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- compat/compat.h
+++ compat/compat.h
@@ -1,3 +1,6 @@
+#ifndef INC_BARNOWL_COMPAT_COMPAT_H
+#define INC_BARNOWL_COMPAT_COMPAT_H
+
 #include "../config.h"
 
 #include <stddef.h>
@@ -5,3 +8,5 @@
 #if !HAVE_DECL_MEMRCHR
 void *memrchr(const void *s, int c, size_t n);
 #endif  /* !HAVE_DECL_MEMRCHR */
+
+#endif /* INC_BARNOWL_COMPAT_COMPAT_H */
