50970e1c5d96255a716f6e51b6545cf70d01e8c7
Jason Gross <jgross@mit.edu>
2011-03-26 04:37:52 -0400

    Show how far you are in a long message
    Addresses most of ticket #119 - it shows how far you are in a long
    message, but not in a message that you're typing.  The display style
    I've somewhat arbitrarily chosen is to replace <truncated> with
    <truncated: $num_lines_through/$total_lines>.
    
    Additionally, the <truncated> part of sepbar has been moved to be next
    to 'SCROLL', which it is most similar to.

--- sepbar.c
+++ sepbar.c
@@ -15,6 +15,7 @@ static void sepbar_redraw(owl_window *w, WINDOW *sepwin, void *user_data)
   const owl_view *v;
   int x, y, i;
   const char *foo, *appendtosepbar;
+  int cur_numlines, cur_totallines;
 
   ml=owl_global_get_msglist(&g);
   v=owl_global_get_current_view(&g);
@@ -49,14 +50,6 @@ static void sepbar_redraw(owl_window *w, WINDOW *sepwin, void *user_data)
   if (strcmp(foo, owl_global_get_view_home(&g)))
       wattron(sepwin, A_REVERSE);
 
-  if (owl_mainwin_is_curmsg_truncated(owl_global_get_mainwin(&g))) {
-    getyx(sepwin, y, x);
-    wmove(sepwin, y, x+2);
-    wattron(sepwin, A_BOLD);
-    waddstr(sepwin, " <truncated> ");
-    wattroff(sepwin, A_BOLD);
-  }
-
   i=owl_mainwin_get_last_msg(owl_global_get_mainwin(&g));
   if ((i != -1) &&
       (i < owl_view_get_size(v)-1)) {
@@ -86,6 +79,16 @@ static void sepbar_redraw(owl_window *w, WINDOW *sepwin, void *user_data)
       waddstr(sepwin, " A-AWAY ");
     }
     wattron(sepwin, A_REVERSE);
+    wattroff(sepwin, A_BOLD);
+  }
+
+  if (owl_mainwin_is_curmsg_truncated(owl_global_get_mainwin(&g))) {
+    getyx(sepwin, y, x);
+    wmove(sepwin, y, x+2);
+    wattron(sepwin, A_BOLD);
+    cur_numlines = owl_global_get_curmsg_vert_offset(&g) + 1;
+    cur_totallines = owl_message_get_numlines(owl_view_get_element(v, owl_global_get_curmsg(&g)));
+    wprintw(sepwin, " <truncated: %d/%d> ", cur_numlines, cur_totallines);
     wattroff(sepwin, A_BOLD);
   }
 
