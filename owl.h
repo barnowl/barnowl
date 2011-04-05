28da2daf4548256b44d1fd586200032e96462d57
David Benjamin <davidben@mit.edu>
2011-04-05 12:54:11 -0400

    Just require stdbool.h and tell ncurses to use it
    Now that Solaris' stdbool.h works for us, we may as well just require
    it. The preamble is repetitive and didn't actually work anyway. (It
    conflicts with ncurses' attempts to redefine bool when stdbool.h is
    unavailable or disabled.)
    
    We may need to add a different preamble later if we care about including
    these headers in C++ (Solaris has a C++-incompatible stdbool.h), but we
    can deal with that later.

--- owl.h
+++ owl.h
@@ -13,19 +13,10 @@
 
 #include "compat/compat.h"
 
-#ifdef HAVE_STDBOOL_H
 #include <stdbool.h>
-#else
-#ifndef HAVE__BOOL
-#define _Bool signed char
-#endif
-#define bool _Bool
-#define false 0
-#define true 1
-#define __bool_true_false_are_defined 1
-#endif  /* HAVE_STDBOOL_H */
 
 #ifndef OWL_PERL
+#define NCURSES_ENABLE_STDBOOL_H 1
 #include <curses.h>
 #include <panel.h>
 #endif
