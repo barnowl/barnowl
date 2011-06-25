37ac3443e4be8b822ab13b34901c88bcb8ed375f
David Benjamin <davidben@mit.edu>
2011-06-25 12:34:33 -0700

    Print out failing lines in prove output
    Makes diagnosing test failures much easier.

--- runtests.sh
+++ runtests.sh
@@ -4,4 +4,4 @@ SRCDIR=`dirname "${0}"`
 export BARNOWL_DATA_DIR="$SRCDIR/perl/"
 export BARNOWL_BIN_DIR="$SRCDIR/"
 
-HARNESS_PERL=./tester exec prove t/
+HARNESS_PERL=./tester exec prove --failures t/
