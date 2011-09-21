0c71c58bb99d8f8814101c336852ba2d47493751
Jason Gross <jgross@mit.edu>
2011-09-21 14:45:34 -0400

    Refactor perl calls through a single method
    I don't know the perl/C interface well enough to figure out the best way
    to standardize the many variants that we use to call perl code.  Perhaps
    we should standardize the error messages, and put less knobs on the
    boilerplate macro.

--- owl_perl.h
+++ owl_perl.h
@@ -3,55 +3,72 @@
 
 #include <stdio.h>
 
-#define OWL_PERL_VOID_CALL (void)POPs;
+/*
+ * This macro defines a convenience wrapper around the boilerplate
+ * of pushing char * arguments on to the stack for perl calling.
+ *
+ * Arguments are
+ * * i     - the counter variable to use, which must be declared prior
+ *           to calling this macro
+ * * argc  - the number of arguments
+ * * argv  - an array of char*s, of length at least argc; the arguments
+ *           to push on to the stack
+ */
+#define OWL_PERL_PUSH_ARGS(i, argc, argv) { \
+  for (i = 0; i < argc; i++) { \
+    XPUSHs(sv_2mortal(owl_new_sv(argv[i]))); \
+  } \
+}
 
 /*
  * This macro defines a convenience wrapper around the boilerplate of
- * calling a method on a perl object (SV*) from C.
+ * the perlcall methods.
  *
  * Arguments are
- * * obj    - the SV* to call the method on
- * * meth   - a char* method name
- * * args   - a code block responsible for pushing args (other than the object)
- * * err    - a string with a %s format specifier to log in case of error
- * * fatalp - if true, perl errors terminate BarnOwl
- * * ret    - a code block executed if the call succeeded
+ * * call       - the line of code to make the perl call
+ * * args       - a code block responsible for pushing args
+ * * err        - a string with a %s format specifier to log in case of error
+ * * fatalp     - if true, perl errors terminate BarnOwl
+ * * discardret - should be true if no return is expected
+ *                (if the call is passed the flag G_DISCARD or G_VOID)
+ * * ret        - a code block executed if the call succeeded
  *
  * See also: `perldoc perlcall', `perldoc perlapi'
  */
-#define OWL_PERL_CALL_METHOD(obj, meth, args, err, fatalp, ret) { \
-    int count; \
-    dSP; \
-    ENTER; \
-    SAVETMPS; \
-    PUSHMARK(SP); \
-    XPUSHs(obj); \
-    {args} \
-    PUTBACK; \
-    \
-    count = call_method(meth, G_SCALAR|G_EVAL); \
-    \
-    SPAGAIN; \
-    \
-    if(count != 1) { \
-      fprintf(stderr, "perl returned wrong count: %d\n", count); \
-      abort();                                                   \
-    } \
-    if (SvTRUE(ERRSV)) { \
-      if(fatalp) { \
-        printf(err, SvPV_nolen(ERRSV)); \
-        exit(-1); \
-      } else { \
-        owl_function_error(err, SvPV_nolen(ERRSV)); \
-        (void)POPs; \
-        sv_setsv(ERRSV, &PL_sv_undef); \
-      } \
+#define OWL_PERL_CALL(call, args, err, fatalp, discardret, ret) { \
+  int count; \
+  dSP; \
+  \
+  ENTER; \
+  SAVETMPS; \
+  \
+  PUSHMARK(SP); \
+  {args} \
+  PUTBACK; \
+  \
+  count = call; \
+  \
+  SPAGAIN; \
+  \
+  if (!discardret && count != 1) { \
+    croak("Perl returned wrong count: %d\n", count); \
+  } \
+  \
+  if (SvTRUE(ERRSV)) { \
+    if (fatalp) { \
+      fprintf(stderr, err, SvPV_nolen(ERRSV)); \
+      exit(-1); \
     } else { \
-      ret; \
+      owl_function_error(err, SvPV_nolen(ERRSV)); \
+      if (!discardret) (void)POPs; \
+      sv_setsv(ERRSV, &PL_sv_undef); \
     } \
-    PUTBACK; \
-    FREETMPS; \
-    LEAVE; \
+  } else if (!discardret) { \
+    ret; \
+  } \
+  PUTBACK; \
+  FREETMPS; \
+  LEAVE; \
 }
 
 #endif /* INC_BARNOWL_OWL_PERL_H */
