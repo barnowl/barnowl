5db8835abc234293110e218bb062b27abaddf399
Anders Kaseorg <andersk@mit.edu>
2012-12-11 00:59:09 -0500

    Allow testing in a separate build directory
    Now we pass ‘make distcheck’.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec env HARNESS_PERL=./tester prove --failures t/
+exec env HARNESS_PERL=./tester prove --failures "${srcdir:=$(dirname "$0")}/t/"
--- t/00-core-tests.t
+++ t/00-core-tests.t
@@ -1,3 +1,2 @@
 #!/usr/bin/env perl
-use File::Basename;
-system(dirname($0) . "/../tester", "--builtin");
+system("./tester", "--builtin");
