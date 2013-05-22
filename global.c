d555aa1e8ee817696b8bfa7aeed40e242c7ec461
Robert Jacobs <rnjacobs@mit.edu>
2013-05-22 13:34:09 -0700

    We should fail fast on -c filsrv, which most people won't have permission to send to

--- global.c
+++ global.c
@@ -855,7 +855,7 @@ void owl_global_setup_default_filters(owl_global *g)
     { "ping", "opcode ^ping$" },
     { "auto", "opcode ^auto$" },
     { "login", "not login ^none$" },
-    { "reply-lockout", "class ^mail$" },
+    { "reply-lockout", "class ^mail$ or class ^filsrv$" },
     { "out", "direction ^out$" },
     { "aim", "type ^aim$" },
     { "zephyr", "type ^zephyr$" },
